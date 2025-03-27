eb4e197eed50d74a3a140d79678e441e4fba10d8
Thomas <przegralek.thomas@gmail.com>
2025-03-27 23:01:37 +0100

    Premier test : recherche de produit et vérification du logo

--- cypress/tests/test01.spec.js
+++ cypress/tests/test01.spec.js
@@ -4,10 +4,17 @@ describe('My first test suite', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/") 
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
-        cy.get('.product').should('have.length,4')
-    })
-
-    it('My second test case', function () {
-        //Test step
+        cy.get('.product:visible').should('have.length',4)
+        //parent child chaining
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').should('have.length',4)
+        cy.get('.products .product').filter(':contains("Cashews")').find('button').click();
+        //This is to print in logs
+        cy.get('.brand').then(function(logoelement)
+        {
+            cy.log(logoelement.text())
+        })
+        //Assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text','GREENKART')
     })
 })
