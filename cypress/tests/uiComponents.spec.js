3d1968f57f7366abf81650bbb6a5e44f9bc550a1
Thomas <przegralek.thomas@gmail.com>
2025-03-28 15:41:48 +0100

    Mise à jour du test UI Components

--- cypress/tests/uiComponents.spec.js
+++ cypress/tests/uiComponents.spec.js
@@ -20,7 +20,7 @@ describe('UI Components', function(){
         cy.get('#autocomplete').type('ind')
         cy.get('.ui-menu-item div').each(($el, index, $list) => {  
             if ($el.text() === "India") {  
-                $el.click() 
+                cy.wrap($el).click({ force: true })  // Force le clic sur l'élément
             }
         })
         cy.get('#autocomplete').should('have.value','India')
@@ -45,5 +45,61 @@ describe('UI Components', function(){
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
         cy.get('[value="radio2"]').check()
         cy.get('[value="radio2"]').should('be.checked')
+    })
+
+    it('Pop up alert without confirmation', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#alertbtn').click()
+        cy.on('window:alert',(str) => {
+            expect(str).to.be.equal('Hello , share this practice page and share your knowledge') //Mocha
+        })
+    })
+
+    it('Pop up with confirmation alert', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('[value="Confirm"]').click()
+        cy.on('window:confirm',(str) => {
+            expect(str).to.be.equal('Hello , Are you sure you want to confirm?')
+        })
+    })
+
+    it('Open Tab vers un autre site internet/domaine', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#opentab').invoke('removeAttr','target').click() //Cypress ne peut pas tester une nouvelle tab , il faut donc supprimer la nouvelle tab et la positionner dans la page actuelle.
+        cy.origin("https://www.qaclickacademy.com/", () => 
+        {
+            cy.get("#navbarSupportedContent a[href*='about']").click()
+            cy.get(".mt-50 h2").should('contain','QAClick Academy')
+        })
+    })
+
+    //it('Open Tab vers le même site internet domaine', function () {
+    //    cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+    //    cy.get('#opentab').invoke('removeAttr', 'target').click()
+    //    cy.url().should('include', 'rahulshettyacademy.com')
+    //    cy.get("#navbarSupportedContent a[href*='about']").click()
+    //    cy.get(".mt-50 h2").should('contain', 'QAClick Academy')
+    //})
+
+    it('Tableau', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('tbody tr td:nth-child(2)').each(($el, index, $list) => {
+            const text=$el.text()
+            if(text.includes("Python"))
+            {
+                cy.get("tbody tr td:nth-child(2)").eq(index).next().then(function(price)
+                {
+                 const priceText=price.text()
+                 expect(priceText).to.equal('25')
+                })
+            }
+        })
+    })
+
+    it('Mouse Over', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('.mouse-hover-content').invoke('show')
+        cy.contains('Top').click()
+        cy.url().should('include','top')
     })
 })
